249139a062a2b93a5f899d5944d8c6d32604218b
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2021-12-04 19:38:25 +0100

    imperative code

--- src/main/java/imperativeDeclarative/Main.java
+++ src/main/java/imperativeDeclarative/Main.java
@@ -1,8 +1,33 @@
 package imperativeDeclarative;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static imperativeDeclarative.Main.Gender.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Integer.parseInt("23",2));
+
+        List<Person> people = List.of(
+                new Person("mina", Female ),
+                new Person("amal", Female),
+                new Person("oma", Male),
+                new Person("kamal", Male)
+                );
+
+        // Imperative
+        List<Person> females = new ArrayList<>();
+
+        for (Person person : people)
+            if(Female.equals(person.gender)) females.add(person);
+        for (Person person : females) System.out.println(person);
+    }
+
+    record Person(String name, Gender gender) { }
+
+    enum Gender {
+        Male,
+        Female
     }
 }
